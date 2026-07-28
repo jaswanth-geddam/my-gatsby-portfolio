d007ff5cac8a75d7f6f5aa5ad7c27c10c054448f
jaswanth-geddam <jaswanth.geddam.lead@gmail.com>
2026-07-28 16:27:56 +0530

    migrate: npm to pnpm

--- run.bat
+++ run.bat
@@ -2,13 +2,13 @@
 cd /d "%~dp0"
 
 echo === Installing ===
-call npm install
+call pnpm install
 
 echo.
 echo === Formatting ===
-call npm run format 2>nul
+call pnpm run format 2>nul
 if %ERRORLEVEL% NEQ 0 echo No format script
 
 echo.
 echo === Starting ===
-call npm run develop
+call pnpm run develop
